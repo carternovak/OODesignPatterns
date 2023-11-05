7cfd5c90b7547f398ce1789a005f78356749ad0e
novakc <novakc@iu.edu>
2023-11-05 15:49:34 -0500

    Code for hw10-12 missing some patterns

---
+++ homework12/Visitor/Obliviator.java
@@ -0,0 +1,8 @@
+public class Obliviator extends Wizard {
+    public Obliviator( String firstname, String lastname ) {
+        super( firstname, lastname );
+    }
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
+    }
+}
